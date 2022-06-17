8c7e16790c8b6693081e8b17b1f970cca285ac33
istatillo <istatillojorayev@gmail.com>
2022-06-17 16:58:08 +0500

    bajarildi

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,11 @@
+import "./footer.css";
+
+const Footer = () => {
+  return (
+    <div className="Footer">
+      <h1 className="oppo">Footer</h1>
+    </div>
+  );
+};
+
+export default Footer;
---
+++ src/components/Header/Header.js
@@ -0,0 +1,12 @@
+function Header() {
+  return <h1 className="oppo">Header</h1>;
+}
+
+const arr = [1, 2, 3, 4, 5];
+let obj = {
+  name: "Istatillo",
+  age: 23,
+};
+
+export default Header;
+export { arr, obj };
